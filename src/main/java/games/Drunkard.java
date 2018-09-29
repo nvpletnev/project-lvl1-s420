413cce7b7511905eda2bc047ee2062a7d5b522b0
nvpletnev <nvpletnev@gmail.com>
2018-09-29 22:02:41 +0300

    Update Drunkard

--- src/main/java/games/Drunkard.java
+++ src/main/java/games/Drunkard.java
@@ -1,75 +1,68 @@
 package games;
 
 import org.apache.commons.math3.util.MathArrays;
-import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 /**
  * Created by nikolaypletnev on 28.09.18.
  */
 public class Drunkard {
-
     private static final int PARS_TOTAL_COUNT = Par.values().length;
     private static final int CARDS_TOTAL_COUNT = PARS_TOTAL_COUNT * Suit.values().length;
 
     //init array card deck
     private static int[] cardDeck = new int[CARDS_TOTAL_COUNT];
 
-    private static int[] beginCursor = new int[2];
-    private static int[] endCursor = new int[2];
-
-    private static int[][] playersCards = new int[2][CARDS_TOTAL_COUNT];
-
 
     public static void main(String... ___) {
 
-        //init card deck
-        for (int i = 0; i < cardDeck.length; i++) {
+        for (int i = 0; i < CARDS_TOTAL_COUNT; i++) {
             cardDeck[i] = i;
         }
 
+        Queue<Integer> player1Cards = new ArrayDeque<>(CARDS_TOTAL_COUNT);
+        Queue<Integer> player2Cards = new ArrayDeque<>(CARDS_TOTAL_COUNT);
+
         MathArrays.shuffle(cardDeck);
 
-        System.arraycopy(cardDeck, 0, playersCards[0],
-                0, cardDeck.length / 2);
+        distributionOfCards(0, cardDeck.length / 2, player1Cards);
+        distributionOfCards(cardDeck.length / 2, cardDeck.length, player2Cards);
 
-        System.arraycopy(cardDeck, cardDeck.length / 2, playersCards[1],
-                0, cardDeck.length / 2);
+        while (!(checkSizeZero(player1Cards, player2Cards))) {
 
-        endCursor[0] = cardDeck.length / 2;
-        endCursor[1] = cardDeck.length / 2;
-        //TODO
+            int card1 = player1Cards.poll() % PARS_TOTAL_COUNT;
+            int card2 = player2Cards.poll() % PARS_TOTAL_COUNT;
 
-        while (endCursor[0] - beginCursor[0] > 0 ||
-                endCursor[1] - beginCursor[1] > 0) {
+            System.out.print("Игрок №1 карта: " + toString(card1) + "; ");
+            System.out.print("Игрок №2 карта: " + toString(card2) + ". ");
 
-            System.out.print("Игрок №1 карта: " + toString(playersCards[0][beginCursor[0]]) + "; ");
-            System.out.print("Игрок №2 карта: " + toString(playersCards[1][beginCursor[1]]) + ". ");
-
-            if (playersCards[0][beginCursor[0]] > playersCards[1][beginCursor[1]]) {
-
+            if (card1 > card2 || (card1 == 0 && card2 == 9)) {
                 System.out.print("Выиграл игрок 1! ");
-
-                endCursor[0] = incrementIndex(endCursor[0]);
-                playersCards[0][endCursor[0]] = playersCards[1][beginCursor[1]];
-                endCursor[0] = incrementIndex(endCursor[0]);
-                playersCards[0][endCursor[0]] = playersCards[0][beginCursor[0]];
-                beginCursor[0] = incrementIndex(beginCursor[0]);
-                beginCursor[1] = incrementIndex(beginCursor[1]);
-
+                player1Cards.offer(card1);
+                player1Cards.offer(card2);
+            } else if (card1 == card2) {
+                player1Cards.offer(card1);
+                player2Cards.offer(card2);
             } else {
                 System.out.print("Выиграл игрок 2! ");
-                endCursor[1] = incrementIndex(endCursor[1]);
-                playersCards[1][endCursor[1]] = playersCards[1][beginCursor[0]];
-                endCursor[1] = incrementIndex(endCursor[1]);
-                playersCards[1][endCursor[1]] = playersCards[1][beginCursor[1]];
-                beginCursor[0] = incrementIndex(beginCursor[0]);
-                beginCursor[1] = incrementIndex(beginCursor[1]);
+                player2Cards.offer(card2);
+                player2Cards.offer(card1);
             }
-            System.out.println("У игрока №1 " + (endCursor[0] - beginCursor[0]) + " карт, " +
-                    "у игрока №2 " + (endCursor[1] - beginCursor[1]));
+
+            System.out.println("У игрока №1 " + player1Cards.size() + " карт, " +
+                    "у игрока №2 " + (player2Cards.size()));
+        }
+
+    }
+
+    private static boolean checkSizeZero(Queue queue1, Queue queue2) {
+        return queue1.size() == 0 || queue2.size() == 0;
+    }
+    private static void distributionOfCards(int beginPosition, int endPosition, Queue queue) {
+        for (int i = beginPosition; i < endPosition; i++) {
+            queue.offer(cardDeck[i]);
         }
     }
 
@@ -83,14 +76,9 @@ public class Drunkard {
         return Par.values()[cardNumber % PARS_TOTAL_COUNT];
     }
 
-    @NotNull
     private static String toString(int cardNumber) {
 
         return getPar(cardNumber) + " " + getSuite(cardNumber);
-    }
-
-    private static int incrementIndex(int i) {
-        return (i + 1) % CARDS_TOTAL_COUNT;
     }
 
     private enum Suit {
@@ -111,6 +99,6 @@ public class Drunkard {
         QUEEN, // Дама
         KING, // Король
         ACE // Туз
-
     }
+
 }
